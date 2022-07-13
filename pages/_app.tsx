8d456e1844d9e516398c3b158bd1d8327b65815e
vanquan25 <minhquan22012@gmail.com>
2022-07-13 11:14:12 +0700

    new

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import Layout from '../component/Layout';
+import { AppPropsWithLayout } from '../component/models/layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const LatoutWrapper = Component.Layout ?? Layout;
+  return (
+  <LatoutWrapper>
+    <Component {...pageProps} />
+  </LatoutWrapper>
+  );
 }
 
 export default MyApp
